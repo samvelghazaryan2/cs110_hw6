cfde0a19fa53b7859269ae6b570d1c6345a96e69
samvelghazaryan2 <samvel.ghazaryan.1998@mail.ru>
2016-11-28 13:40:42 +0400

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -2,18 +2,18 @@ import com.shape.*;
 import com.utils.Math;
 public class Driver{
     public static void main(String[] args) {
-        int sideLenght = 100;    //Square
-        int width = 50;          //Rectangle
-        int height = 30;
-        double number1 = 15.6;   //Factorial(Recursion)
-        double number2 = 6.8;    //Factorial(ForLoop)
-        Square sq = new Square(sideLenght);
-        long SquareArea = sq.getArea();
-        System.out.println("Area of Square with side lenght of "+ sideLenght +" equals to "+SquareArea);
-        Rectangle rec = new Rectangle(width,height);
-        long RectangleArea = rec.getArea();
-        System.out.println("Area of Rectangle with width of "+ width +" and height of "+ height +" equals to " +RectangleArea);
-        System.out.println("Factorial of integer part of "+ number1 +" is " + Math.factorial(number1));
-        System.out.println("Factorial of integer part of "+ number2 +" is " + Math.factorialLoop(number2));
+        int sideLenght = 100;    
+        int w = 50;         
+        int h = 80;
+        double f1 = 15.6;   
+        double f2 = 6.8;    
+        Square s = new Square(sideLenght);
+        long SquareArea = s.getArea();
+        System.out.println(SquareArea);
+        Rectangle r = new Rectangle(w,h);
+        long RectangleArea = r.getArea();
+        System.out.println(RectangleArea);
+        System.out.println(Math.factorial(f1));
+        System.out.println(Math.factorialLoop(f2));
     }
 }
